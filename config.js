828d729c40d113cb188b59e8d0c6671fd52c8a02
stans <834410151@qq.com>
2019-09-15 20:59:26 +0800

    feat connect mysql

--- config.js
+++ config.js
@@ -1,9 +1,23 @@
 const dev = {
-  HOSTPORT: 3000
+  HOSTPORT: 3000,
+  MYSQL: {
+    host: 'localhost',
+    user: 'root',
+    password: '111111',
+    database: 'fundManage',
+    port: 3306
+  }
 }
 
 const prod = {
-  HOSTPORT: 4000
+  HOSTPORT: 4000,
+  MYSQL: {
+    host: 'localhost',
+    user: 'root',
+    password: '111111',
+    database: 'fund',
+    port: 3306
+  }
 }
 
 module.exports = process.env.NODE_ENV === 'development' ? dev : prod
